1355e3ec5480cf6d88dbe54468fc4b3b9d48bf1d
AbdoNJ <mr.nejjari.abdelhafid@gmail.com>
2023-04-08 02:26:18 +0000

    fix linters errors

--- test/controllers/api/v1_controller_test.rb
+++ test/controllers/api/v1_controller_test.rb
@@ -1,7 +1,7 @@
-require 'test_helper'
+require "test_helper"
 
 class Api::V1ControllerTest < ActionDispatch::IntegrationTest
-  test 'should get message' do
+  test "should get message" do
     get api_v1_message_url
     assert_response :success
   end
